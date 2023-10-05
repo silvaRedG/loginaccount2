ab251571f3d3623f8e9d344f704219f9b2387f82
silvaRedG <matheus.silva@redgtech.com.br>
2023-10-05 02:24:15 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import People from "./assets/people.svg";
-import Arrow from "./assets/arrow.svg"
+import React, { useState } from "react";
+import People from "./assets/america-1861417.svg";
+import Arrow from "./assets/arrow.svg";
+import Trash from "./assets/trash.svg";
 
 import {
   Container,
@@ -10,45 +11,117 @@ import {
   InputLabel,
   Input,
   Button,
+  Users,
 } from "./styles";
 
 function App() {
+  /*
+  
+  -> nome variavel
+ -> nome da função que vai atualizar a variavel
+-> dentro do usestate valor inicial da aplicação 
 
-const user = [
-  {id: Math.random, name:"Matheus", age:28},
-  {id: Math.random, name:"Matheus", age:28},
-]
+2-> trecho dois vou criar um estado pra ser chamado,
+evento onchange dos inputs pra cada campo
+  
+  */
+
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [email, setEmail] = useState();
+
+  /*função onde clico no botão ele criar usuario e joga
+  usuarios criado pra dentro do array
+  
+  estado no react é imutavel*/
+
+  /*
+      react hook = ferramenta auxiliar a criar estado no react
+
+      exemplo abaixo de um comando react hook
+
+      add dentro do user um array com objeto dentro
+*/
+
+  /* no js não preciso repetir os valores
+propriedades mesmo nome da variavel se coloca uma vez só */
+
+/*speed operator
+
+ 
 
 
+*/
 
+  function addUser() {
+    setUsers([...users,{ id: Math.random(), name, age, email }]);
+  }
 
+  /*
+     Funções OnChange para alterar campos na aplicação
+*/
+  function nameInput(event) {
+    setName(event.target.value);
+  }
+
+  function ageInput(event) {
+    setAge(event.target.value);
+  }
+
+  function emailInput(event) {
+    setEmail(event.target.value);
+  }
+
+  /*
+     Fim
+*/
+
+  // Estado => Variável
 
   return (
     <Container>
-
-      <Image alt="logo-imagem" src={People} />
-
+      <Image alt="logo-img" src={People} />
       {/* Criando os itens do forms da applicação*/}
       <ContainerItens>
-        <H1>Olá!!!</H1>
+        <H1>Welcome - React Forms</H1>
 
         <InputLabel>Name</InputLabel>
-        <Input placeholder="Name" autoFocus />
+        <Input onChange={nameInput} placeholder="Name" autoFocus />
 
         <InputLabel>Age</InputLabel>
-        <Input placeholder="Age" />
+        <Input onChange={ageInput} placeholder="Age" />
 
         <InputLabel>Email</InputLabel>
-        <Input placeholder="Email" />
+        <Input onChange={emailInput} placeholder="Email" />
 
-        <Button>Register <img alt="arrow" src={Arrow}/></Button>
+        <Button onClick={addUser} type="submit">
+          Register <img alt="seta" src={Arrow} />
+        </Button>
 
-    <ul>
-      <li>
-        {user[0].name} - {user[0].age}
-      </li>
-    </ul>
+        {/*Loop intera array - > no react utilizao o map pra mapear
+ item por item, inserido a função js map dentro da ul para retornar os users
+ obs: no retorno da função estou inserindo HTML*/}
 
+        {/*Sempre que for interar um item no react,
+necessario ter um função key pra armazenar o id, 
+para quando for deletar ele saber qual foi o item
+
+*no campo abaixo foi criado um html onde inserimos uma UL
+dentro dela utilizamos o metodo map para mapear o array e da função id*/}
+
+        <ul>
+          {users.map((users) => (
+            <Users key={users.id}>
+              <p>{users.name}</p>
+              <p>{users.age}</p>
+              <p>{users.email}</p>
+              <button>
+                <img alt="trash" src={Trash} />
+              </button>
+            </Users>
+          ))}
+        </ul>
       </ContainerItens>
       {/* Fim dos Itens */}
     </Container>
--- src/styles.js
+++ src/styles.js
@@ -1,106 +1,177 @@
 import styled from "styled-components";
-import Background from "./assets/background1.svg";
+
+
+
+/*para inserir a imagem devo declarar o import primeiro
+  em seguida atribuir a imagem dentro de uma url
+  no formato parecido com template string*/
 
 export const Container = styled.div`
-  background: url("${Background}");
-  background-size: cover;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  
+background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
 
-  min-height: 100vh;
 
-  gap: 40px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 40px;
+
+min-height: 100vh;
+
+
+
+
 `;
 
 export const Image = styled.img`
-  margin-top: 30px;
-`;
+
+margin-top: 30px;
+`
 
 export const ContainerItens = styled.div`
-  border-radius: 61px 61px 0px 0px;
-  background: linear-gradient(
-    157deg,
-    rgba(255, 255, 255, 0.6) 0.84%,
-    rgba(255, 255, 255, 0.6) 0.85%,
-    rgba(255, 255, 255, 0.15) 100%
-  );
 
-  min-height: 100vh;
+border-radius: 61px 61px 0px 0px;
+background: linear-gradient(157deg, 
+    rgba(255, 255, 255, 0.60) 0.84%,
+    rgba(255, 255, 255, 0.60) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%);
 
-  padding: 50px 36px;
 
-  display: flex;
-  flex-direction: column;
-`;
+    padding: 50px 36px;
+
+    display: flex;
+    flex-direction: column;
+    
+    min-height: 100vh;
+
+`
+
 
 export const H1 = styled.h1`
-  color: #fff;
-  text-align: center;
-  font-size: 34px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
 
-  margin-bottom: 80px;
+color: #1C1C1C;
+text-align: center;
+font-size: 34px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+
+margin-bottom: 80px;
+
 `;
 
 export const InputLabel = styled.p`
-  color: #eeeeee;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: bold;
-  line-height: 22px; /* 122.222% */
-  letter-spacing: -0.408px;
+  
+color: #eeeeee;
+font-size: 18px;
+font-style: normal;
+font-weight: bold;
+line-height: 27px; /* 122.222% */
+letter-spacing: -0.408px;
+margin-left: 25px;
+
+
+  
 `;
 
 export const Input = styled.input`
-  border-radius: 14px;
-  background: rgba(255, 255, 255, 0.25);
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
-  width: 342px;
-  height: 58px;
-  padding-left: 25px;
-  margin-bottom: 35px;
+border-radius: 14px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
-  border: none;
-  outline: none;
+width: 342px;
+height: 58px;
 
-  font-style: normal;
-  font-weight: normal;
-  font-size: 20px;
-  line-height: 28px;
-`;
+border: none;
+outline: none;
+
+padding-left: 25px; /* letra dentro do input recuar */ 
+
+color: #1C1C1C;
+font-style: normal;
+font-weight: normal;
+font-size: 18px;
+line-height: 28px;
+
+
+margin-bottom: 35px;
+
+
+`
 
 export const Button = styled.button`
-  border-radius: 14px;
-  background: var(--templates-5-color-1, rgba(0, 0, 0, 0.8));
 
-  width: 342px;
-  height: 74px;
-  color: #eeeeee;
+border-radius: 14px;
+background: var(--templates-5-color-1, rgba(0, 0, 0, 0.80));
 
-  font-style: normal;
-  font-weight: bold;
-  font-size: 17px;
-  line-height: 28px;
+width: 342px;
+height: 74px;
+margin-top: 20px;
 
-  border: none;
-  background: rgba(0,0,0,0.8);
-  border-radius: 14px;
-  outline: none;
-  cursor: pointer;
+cursor: pointer;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  
+color: #FFF;
+font-style: normal;
+font-size: 17px;
+font-weight: 700;
+line-height: 28px; /* 164.706% */
 
-  &:hover{
-    opacity: 0.8;
-  }
+border: none;
+
+/* Aplicando display flex pra ajustar o button e img dentro dele */
+display: flex;
+justify-content: center;
+align-items: center;
+gap:20px;
+
+
+/* Aplicando efeito hover dentro do react, sempre utilizar o "&"*/
+
+&:hover{
+  opacity: 0.5;
+}
+
+&:active{
+  opacity: 0.9;
+}
+
 `
-;
+
+export const Users = styled.li`
+
+display: flex;
+justify-content: space-around;
+align-items: center;
+
+border-radius: 14px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+width: 342px;
+height: auto;
+
+outline: none;
+border: none;
+
+margin-top: 20px;
+
+
+p{
+  color: #fff;
+  font-size: 20px;
+  text-align: left;
+}
+
+button{
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+
+
+`
+
+
